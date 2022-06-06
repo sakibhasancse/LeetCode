6c3d2cc0478cc64cd97902df7276fba4e5f59dc5
SakibHasanCse <sakibhasan9641@gmail.com>
2022-06-06 23:28:08 +0600

    Added palindrome

--- Easy.js
+++ Easy.js
@@ -33,7 +33,7 @@
 // console.log(romanToInt("LVIII"))
 
 // Answer 02
-var romanToInt = function(s) {
+var romanToInt = function (s) {
     let answer = 0;
     const values = {
         I: 1,
@@ -44,12 +44,37 @@ var romanToInt = function(s) {
         D: 500,
         M: 1000
     }
-    
-  return s.split('').reduce((prev, curr, i) => {
-        return prev + (i >=s.length -1 || values[s[i+1]] <= values[s[i]] ? values[s[i]] : -values[s[i]]);
+
+    return s.split('').reduce((prev, curr, i) => {
+        return prev + (i >= s.length - 1 || values[s[i + 1]] <= values[s[i]] ? values[s[i]] : -values[s[i]]);
     }, 0);
-        
-    
+
+
 };
 
 console.log(romanToInt("LVIII"))
+
+
+// 9. Palindrome Number
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    if (x < 0 || x % 10 === 0 && x !== 0) {
+        return false;
+    }
+    let reversedNum = 0;
+    let temp = x
+
+    while (x < reversedNum) {
+        reversedNum = reversedNum * 10 + temp % 10
+        temp = temp / 10
+        console.log({ reversedNum, temp })
+    }
+    
+    return x === reversedNum
+};
+
+console.log(isPalindrome(121))
