7304ece94a5ffb28388ba8bf1fcff6c331faeec6
SakibHasanCse <sakibhasan9641@gmail.com>
2022-06-07 23:31:57 +0600

    Solved roman number to int

--- Easy.js
+++ Easy.js
@@ -33,24 +33,22 @@
 // console.log(romanToInt("LVIII"))
 
 // Answer 02
-var romanToInt = function (s) {
-    let answer = 0;
-    const values = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000
-    }
+// var romanToInt = function (s) {
+//     let answer = 0;
+//     const values = {
+//         I: 1,
+//         V: 5,
+//         X: 10,
+//         L: 50,
+//         C: 100,
+//         D: 500,
+//         M: 1000
+//     }
 
-    return s.split('').reduce((prev, curr, i) => {
-        return prev + (i >= s.length - 1 || values[s[i + 1]] <= values[s[i]] ? values[s[i]] : -values[s[i]]);
-    }, 0);
-
-
-};
+//     return s.split('').reduce((prev, curr, i) => {
+//         return prev + (i >= s.length - 1 || values[s[i + 1]] <= values[s[i]] ? values[s[i]] : -values[s[i]]);
+//     }, 0);
+// };
 
 // console.log(romanToInt("LVIII"))
 
@@ -78,3 +76,43 @@ var isPalindrome = function (x) {
 };
 
 console.log(isPalindrome(121))
+
+
+// 13. Roman to Integer
+
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var romanToInt = function (s) {
+    let symbol = [{ I: 1 }, { V: 5 }, { X: 10 }, { L: 50 }, { C: 100 }, { D: 500 }, { M: 1000 }]
+    let number = 0;
+    let lastValue;
+
+    s.split('').forEach(item => {
+        const currentNumber = symbol.find((a => a[item]))[item]
+        if (lastValue) {
+            const lastNumber = symbol.find((a => a[lastValue]))[lastValue]
+            if ((lastValue === 'I' && (item === 'X' || item === 'V')) ||
+                (lastValue === 'X' && (item === 'C' || item === 'L')) ||
+                (lastValue === 'C' && (item === 'M' || item === 'D'))
+            ) {
+                const newNumber = (currentNumber - lastNumber)
+                number += newNumber - lastNumber
+            } else number += currentNumber
+
+        } else {
+            number += currentNumber
+        }
+        lastValue = item
+    })
+
+    return number
+};
+
+console.log(romanToInt('MCMXCIV'))
