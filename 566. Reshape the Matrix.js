2b8febec3e9e4c5cf81057a3c9521fe119c6e283
SakibHasanCse <sakibhasan9641@gmail.com>
2022-06-28 00:29:27 +0600

    Added new solutions

--- 566. Reshape the Matrix.js
+++ 566. Reshape the Matrix.js
@@ -5,19 +5,42 @@
  * @return {number[][]}
  */
 
-const chunk = (arr, n) => arr.length ? [arr.slice(0, n), ...chunk(arr.slice(n), n)] : [];
+// const chunk = (arr, n) => arr.length ? [arr.slice(0, n), ...chunk(arr.slice(n), n)] : [];
+
+// var matrixReshape = function (mat, r, c) {
+//   const newMat = [].concat.apply([], mat);
+//   if (newMat.length === c && mat.length === r) return mat
+//   else {
+//     const maximumMat = newMat.slice(0, c)
+//     const numberOfArray = maximumMat.length / r
+//     if (maximumMat.length <= r) return maximumMat
+//     const value = chunk(maximumMat, numberOfArray)
+//     return value
+//   }
+// };
+
 
 var matrixReshape = function (mat, r, c) {
-  const newMat = [].concat.apply([], mat);
-  if (newMat.length === c && mat.length === r) return mat
+  if ((r * c != mat.length * mat[0].length) || mat.length === r) return mat
   else {
-    const maximumMat = newMat.slice(0, c)
-    const numberOfArray = maximumMat.length / r
-    if (maximumMat.length <= r) return maximumMat
-    const value = chunk(maximumMat, numberOfArray)
-    return value
+    let newMat = []
+    let columns = 0;
+    let values = []
+    for (var i = 0; i < mat.length; i++) {
+      for (var j = 0; j < mat[0].length; j++) {
+        values.push(mat[i][j])
+        columns++
+        if (columns == c) {
+          newMat.push(values)
+          columns = 0
+          values = []
+        }
+      }
+    }
+    return newMat
   }
 };
+
 console.log(matrixReshape([[1, 2], [3, 4]], 2, 4))
 console.log(matrixReshape([[1, 2], [3, 4]], 1, 4))
-console.log(matrixReshape([[1, 2], [3, 4]], 4, 1))
+console.log(matrixReshape([[1, 2], [3, 4]], 4, 4))
