c3e9a1c7fb9f0ab2f2a6b65c2ae95977db1fc135
SakibHasanCse <sakibhasan9641@gmail.com>
2022-06-27 23:52:26 +0600

    Added solution

--- 566. Reshape the Matrix.js
+++ 566. Reshape the Matrix.js
@@ -4,6 +4,20 @@
  * @param {number} c
  * @return {number[][]}
  */
-var matrixReshape = function (mat, r, c) {
 
+const chunk = (arr, n) => arr.length ? [arr.slice(0, n), ...chunk(arr.slice(n), n)] : [];
+
+var matrixReshape = function (mat, r, c) {
+  const newMat = [].concat.apply([], mat);
+  if (newMat.length === c && mat.length === r) return mat
+  else {
+    const maximumMat = newMat.slice(0, c)
+    const numberOfArray = maximumMat.length / r
+    if (maximumMat.length <= r) return maximumMat
+    const value = chunk(maximumMat, numberOfArray)
+    return value
+  }
 };
+console.log(matrixReshape([[1, 2], [3, 4]], 2, 4))
+console.log(matrixReshape([[1, 2], [3, 4]], 1, 4))
+console.log(matrixReshape([[1, 2], [3, 4]], 4, 1))
