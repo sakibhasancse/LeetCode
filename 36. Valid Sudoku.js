7c3ab21cc3ee013982dd536b63d88446922de923
SakibHasanCse <sakibhasan9641@gmail.com>
2022-07-04 00:38:08 +0600

    Added solution

--- 36. Valid Sudoku.js
+++ 36. Valid Sudoku.js
@@ -4,30 +4,27 @@
  */
 var isValidSudoku = function (board) {
 
-  let isValid = true;
-  let currentValue = 0
-  let lastIndex = 0
+  let hashSet = new Set();
 
-  for (var i = 1; i < board.length; i++) {
-    const current = board[i]
-    const prevValue = board[i - 1]
-    const duplicates = current.filter((item, index) => Number(item) && index !== current.indexOf(item));
- 
-    if (duplicates.length > 0) return isValid = false
+  for (var i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] !== '.') {
+        if (hashSet.has('row' + i + board[i][j]) || hashSet.has('col' + j + board[i][j]) || hashSet.has('box' + (i / 3 * 3 + j / 3) + board[i][j])) {
+          return false;
+        } else {
+          hashSet.add('row' + i + board[i][j])
+          hashSet.add('col' + j + board[i][j])
+          hashSet.add('box' + (i / 3) * 3 + (j / 3) + board[i][j])
+        }
+      }
 
-    for (let j = 0; j < current.length; j++) {
-      const element = current[j];
-      console.log({ element })
     }
-
-    if (lastIndex + 3 === i) lastIndex = i
-
   }
-  console.log({ isValid })
-  return isValid
+  console.log({ hashSet })
+  return true
 };
 
-isValidSudoku([
+const result = isValidSudoku([
   ["5", "3", ".", ".", "7", ".", ".", ".", "."]
   , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
   , [".", "9", "8", ".", ".", ".", ".", "6", "."]
@@ -37,3 +34,17 @@ isValidSudoku([
   , [".", "6", ".", ".", ".", ".", "2", "8", "."]
   , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
   , [".", ".", ".", ".", "8", ".", ".", "7", "9"]])
+
+
+const result2 = isValidSudoku([
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."]
+  , ["5", ".", ".", "1", "9", "5", ".", ".", "."]
+  , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+  , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+  , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+  , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+  , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+  , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+  , [".", ".", ".", ".", "8", ".", ".", "7", "9"]])
+
+console.log({ result, result2 })
