a3e8e48615e40d4833ab7b75e9844b9c90c16200
SakibHasanCse <sakibhasan9641@gmail.com>
2022-07-29 12:43:31 +0600

    Addeed hashmap solution

---
+++ 242. Valid Anagram.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+
+// var isAnagram = function (s, t) {
+//   if (s.length !== t.length) return false
+
+//   const firstString = s.split('').sort().join('')
+//   const secondString = t.split('').sort().join('')
+//   if (firstString === secondString) return true
+//   return false
+
+// };
+
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false
+
+  let sCounts = {}, tCounts = {}
+  for (let word = 0; word < s.length; word++) {
+    sCounts[s[word]] = 1 + sCounts[s[word]] || 1
+    tCounts[t[word]] = 1 + tCounts[t[word]] || 1
+  }
+  for (let rr in sCounts) {
+    if (sCounts[rr] !== tCounts[rr]) return false
+  }
+  return true
+
+};
+console.log(isAnagram("anagram", "mnagraa"))
