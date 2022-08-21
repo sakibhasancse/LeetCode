c61ee48a2c0f5df676d69996d7c7b6743e6cee87
SakibHasanCse <sakibhasan9641@gmail.com>
2022-08-22 00:43:11 +0600

    Added mutiple solution

---
+++ 1512. Number of Good Pairs.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var numIdenticalPairsOne = function (nums) {
+  let numberOfGoodPair = 0;
+  for (let num = 0; num < nums.length; num++) {
+    const elementOne = nums[num];
+    for (let item = num + 1; item < nums.length; item++) {
+      const element = nums[item];
+      if (elementOne == element) numberOfGoodPair++
+    }
+  }
+  return numberOfGoodPair
+
+};
+
+
+var numIdenticalPairs = function (nums) {
+  let numberOfGoodPair = 0;
+  let hash = {}
+  for (let num = 0; num < nums.length; num++) {
+    const element = nums[num];
+    if (hash[element]) hash[element]++
+    else hash[element] = 1
+  }
+  for (const ddd in hash) {
+    const element = hash[ddd];
+    console.log({ element })
+    numberOfGoodPair += element * (element - 1) / 2
+  }
+  console.log({ hash })
+
+  return numberOfGoodPair
+
+};
+// Example 1:
+
+// Input: nums = [1, 2, 3, 1, 1, 3]
+// Output: 4
+// Explanation: There are 4 good pairs(0, 3), (0, 4), (3, 4), (2, 5) 0 - indexed.
+//   Example 2:
+
+// Input: nums = [1, 1, 1, 1]
+// Output: 6
+// Explanation: Each pair in the array are good.
+//   Example 3:
+
+// Input: nums = [1, 2, 3]
+// Output: 0
