2a0598f66ba49c7d3230f8f0fe4ef81c408da727
SakibHasanCse <sakibhasan9641@gmail.com>
2022-08-29 00:23:22 +0600

    Runtime: 83 ms, faster than 68.37% of JavaScript online submissions for Time Needed to Buy Tickets.

---
+++ 2073. Time Needed to Buy Tickets.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function (tickets, k) {
+  let totalTime = 0;
+  let n = tickets.length;
+  let i = 0;
+  while (tickets[k] != 0) {
+    if (tickets[i % n] == 0) {
+      //If someone needs no tickets we skip that person
+      i++;
+      continue;
+    }
+    tickets[i % n]--;
+    i++;
+    totalTime++
+  }
+  return totalTime
+};
