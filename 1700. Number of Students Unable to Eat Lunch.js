ed0cc67689bf28f332bc96cadbac1ce1bb06439e
SakibHasanCse <sakibhasan9641@gmail.com>
2022-09-02 00:23:27 +0600

    Runtime: 517 ms, faster than 5.00% of JavaScript online submissions for Number of Students Unable to Eat Lunch.

---
+++ 1700. Number of Students Unable to Eat Lunch.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function (students, sandwiches) {
+  while (students.length && students.includes(sandwiches[0])) {
+    if (students[0] === sandwiches[0]) {
+      students.shift()
+      sandwiches.shift()
+    } else students.push(students.shift())
+  }
+  return students.length;
+};
