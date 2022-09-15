69535e1b735a8776046e8a025f8e6456433fda54
SakibHasanCse <sakibhasan9641@gmail.com>
2022-09-16 01:59:59 +0600

    Added deleteDuplicates solution

---
+++ 206. Reverse Linked List.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(!head || !head.next) return head;
+    let previous = null;
+    while(head){
+        nextNode = head.next;
+        head.next = previous
+        previous = head
+        head = nextNode
+    }
+
+    return previous
+}}
