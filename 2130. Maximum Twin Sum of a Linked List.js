20c73a6076ba0bcb84ed454764715fda2b16e349
Sakib Hasan <sakibhasan9641@gmail.com>
2022-09-27 00:53:48 +0600

    faster than 33.86% of JavaScript online submissions for Maximum Twin Sum of a Linked List.

--- 2130. Maximum Twin Sum of a Linked List.js
+++ 2130. Maximum Twin Sum of a Linked List.js
@@ -9,17 +9,17 @@
  * @param {ListNode} head
  * @return {number}
  */
-var pairSum = function(head) {
+var pairSum = function (head) {
     let allNumber = [];
-    while(head){
-        allNumber.push(head.val)
-        head =head.next
+    while (head) {
+        allNumber.push(head.val);
+        head = head.next;
     }
 
     let maximumSum = 0;
-    for(var i=0; i<allNumber.length / 2; i++){
-        maximumSum = Math.max(maximumSum, allNumber[i] + allNumber[allNumber.length - 1 -i])
+    for (var i = 0; i < allNumber.length / 2; i++) {
+        maximumSum = Math.max(maximumSum, allNumber[i] + allNumber[allNumber.length - 1 - i]);
     }
 
-    return maximumSum
+    return maximumSum;
 };
